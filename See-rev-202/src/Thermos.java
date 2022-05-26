9758ae56146ba520ec818f829cbcd507a57b5e26
admin <admin@Bcalab4-m15>
2022-05-26 09:47:24 +0530

    commit

---
+++ See-rev-202/src/Thermos.java
@@ -0,0 +1,11 @@
+
+public class Thermos 
+{
+	public Thermos() {
+		
+	}
+	public static void main(String[] args) {
+		System.out.println("plkmjinuhb");
+		System.out.println("syuigdfsyuguasgfuy");
+	}
+}
